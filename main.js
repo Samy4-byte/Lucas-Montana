7ad5d2949ea63c11dda062481a716fa80ebe40ac
Samy4-byte <samatbayizov@gmail.com>
2023-04-03 16:17:19 +0600

    add adaptation

--- main.js
+++ main.js
@@ -1,27 +1,113 @@
 const pics = [
-  'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80',
-  'https://images.unsplash.com/photo-1647891941746-fe1d53ddc7a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-  'https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80',
-  'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80',
-  'https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
-  'https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-  'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80",
+  "https://images.unsplash.com/photo-1647891941746-fe1d53ddc7a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+  "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80",
+  "https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+  "https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80",
+  "https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80",
+  "https://images.unsplash.com/photo-1647891941746-fe1d53ddc7a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+  "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80",
+  "https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+  "https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80",
+  "https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80",
+  "https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
 ]
 
-
-const gallery = document.querySelector('.container')
-const imageBox = document.createElement('div')
-
-// imageBox.style.height = 300 + 'px'
-
+const gallery = document.querySelector(".portfolio")
+const imageBox = document.createElement("div")
 gallery.append(imageBox)
+imageBox.classList.add('img-box')
 
 
-pics.forEach(pic => {
-  const img = document.createElement('img')
+
+const modalBtn = document.querySelector(".modal-btn")
+
+pics.forEach((pic) => {
+  const imgWrapper = document.createElement("div")
+  imgWrapper.classList.add("imgWrapper")
+  const img = document.createElement("img")
+  img.classList.add("img-item")
   img.src = pic
-  imageBox.appendChild(img)
-  img.classList.add('img-item')
+  imgWrapper.appendChild(img)
+  imageBox.appendChild(imgWrapper)
+})
+
+const images = document.querySelectorAll('.img-item')
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', function () {
+
+    const wrapperImg = document.createElement('div')
+    document.body.appendChild(wrapperImg)
+    wrapperImg.classList.add('image-modal-wrapper')
+
+    const modalImg = document.createElement('img')
+    modalImg.setAttribute('src', this.getAttribute('src'))
+    modalImg.style.width = '50%'
+    modalImg.style.height = '80%'
+    modalImg.style.objectFit = 'cover'
+
+    const closeBtn = document.createElement('button')
+    closeBtn.classList.add('close-btn')
+    closeBtn.textContent = 'close'
+    wrapperImg.appendChild(closeBtn)
+
+    wrapperImg.appendChild(modalImg)
+    
+
+    closeBtn.addEventListener('click', function () {
+      wrapperImg.remove()
+    })
+
+    wrapperImg.addEventListener('click', () => {
+      wrapperImg.remove()
+    })
+  
+  })
+}
+
+const form = document.querySelector("form")
+
+const wrapper = document.querySelector(".wrapper")
+
+const modal = document.querySelector(".modal")
+
+const btn = document.querySelector(".btn")
 
 
+btn.addEventListener("click", () => {
+  wrapper.style.display = "flex"
+  modal.style.display = "flex"
+})
+
+const iconX = document.querySelector(".icon-x")
+
+iconX.addEventListener("click", () => {
+  wrapper.style.display = "none"
+  modal.style.display = "none"
+  form.reset()
+})
+
+wrapper.addEventListener("click", () => {
+  wrapper.style.display = "none"
+  modal.style.display = "none"
+  form.reset()
+})
+
+
+modalBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  let user = {}
+  user.name = form.elements.name.value
+  user.email = form.elements.email.value
+  user.about = form.elements.text.value
+  console.log(user)
+  wrapper.style.display = "none"
+  modal.style.display = "none"
+  form.reset()
 })
